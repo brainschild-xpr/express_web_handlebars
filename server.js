90c7234f0c31cc39e75cf30d01c18610102c0a98
XPR <shungoh.martin@gmail.com>
2020-01-26 18:51:49 +0300

    css

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3000
 const main_layout = path.join(__dirname, 'views/main_layout')
 const partial_layout = path.join(__dirname, 'views/partials')
 const public = path.join(__dirname, 'public')
-// console.log(public)
+    // console.log(public)
 
 app.use(express.static('public'));
 
@@ -18,18 +18,19 @@ const hbsE = exp_hbs.create({
     partialsDir: partial_layout,
     // create customer helper
     helpers: {
-        calculation: function (value) {
+        calculation: function(value) {
             return value + 50
-        }, list: function (value, options) {
+        },
+        list: function(value, options) {
             console.log("Passing Values", value);
             return '<h3>' + options.fn({ test: value, another: 'thing' }) + '</h3>'
 
         },
-        people: function (value, options) {
+        people: function(value, options) {
             //value = people
-            let out = "<ul>"
+            let out = "<ul='1'>"
             for (let i = 0; i < value.length; i++) {
-                out = out + '<li>' + options.fn(value[i]) + '</li>'
+                out = out + '<p>' + options.fn(value[i]) + '</p>'
             }
             return out + '</ul>'
 
@@ -43,9 +44,10 @@ app.set('view engine', 'hbs')
 
 
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     // res.status(200).json({ message: 'Welcome to Home' })
     res.render('index', {
+        style: 'index.css',
         title: 'HomePage',
         isTrue: true,
         connectedUser: false,
@@ -64,17 +66,19 @@ app.get('/', function (req, res) {
     })
 })
 
-app.get('/about', function (req, res) {
+app.get('/about', function(req, res) {
     // res.status(200).json({ message: 'Welcome to Home' })
     res.render('about', {
+        style: 'about.css',
         title: 'AboutPage',
         name: 'BCXPR',
         author: 'Shungoh'
     })
 })
 
-app.get('/team', function (req, res) {
+app.get('/team', function(req, res) {
     res.render('team', {
+        style: 'team.css',
         programmers: [
             'Martin',
             'Ian',
@@ -96,8 +100,9 @@ app.get('/team', function (req, res) {
     })
 })
 
-app.get('/food', function (req, res) {
+app.get('/food', function(req, res) {
     res.render('food', {
+        style: 'food.css',
         food: [
             { fruits: ['passion', 'orange', 'apple'] },
             { vegetables: ['spinach', 'cauliflower', 'kales'] },
@@ -107,6 +112,6 @@ app.get('/food', function (req, res) {
     })
 })
 
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log('Server listening at port:', PORT)
 })
