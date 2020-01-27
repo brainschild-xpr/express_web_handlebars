134f649e5098a475f07c721b49a7a092e178e02e
XPR <shungoh.martin@gmail.com>
2020-01-27 03:49:30 +0300

    added index.js Route

---
+++ routes/index.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//     res.render('index', { title: 'Express' });
+//   });
+
+router.get('/', function (req, res) {
+    res.render('index', {
+        style: 'index.css',
+        title: 'HomePage',
+        isTrue: true,
+        connectedUser: false,
+
+        duoCurly: '<h3>Welcome to Duo Curly</h3>',
+        trioCurly: '<h3>Welcome to Trio Curly</h3>',
+        author: {
+            firstname: 'Malory',
+            lastname: 'Archer',
+            employees: {
+                first: 'Sterling Archer',
+                second: 'Lana Kane',
+                third: 'Cyril Figgis'
+            }
+        },
+    })
+})
+
+router.get('/about', function (req, res) {
+    // res.status(200).json({ message: 'Welcome to Home' })
+    res.render('about', {
+        style: 'about.css',
+        title: 'AboutPage',
+        name: 'BCXPR',
+        author: 'Shungoh'
+    })
+})
+
+router.get('/team', function (req, res) {
+    res.render('team', {
+        style: 'team.css',
+        programmers: [
+            'Martin',
+            'Ian',
+            'Luffy',
+            'Steve'
+        ],
+        accountant: {
+            name: 'Malory Archer',
+            username: 'ilovemoney',
+            office_number: 'b40',
+            phone: '0785746334'
+        },
+        people: [
+            { firstname: 'Janice', secondname: 'Kemunto' },
+            { firstname: 'James', secondname: 'Kaimoe' },
+            { firstname: 'Archie', secondname: 'Malowa' },
+        ]
+
+    })
+})
+
+router.get('/food', function (req, res) {
+    res.render('food', {
+        style: 'food.css',
+        food: [
+            { fruits: ['passion', 'orange', 'apple'] },
+            { vegetables: ['spinach', 'cauliflower', 'kales'] },
+            { junk: ['pizza', 'burger', 'sandwitch'] },
+            { beverages: ['coffee', 'tea', 'choco'] }
+        ]
+    })
+})
+
+router.get('/admin', function (req, res) {
+    res.render('admin', {
+        style: 'admin.css'
+    })
+})
+
+  module.exports = router
--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const PORT = 3000
 const main_layout = path.join(__dirname, 'views/main_layout')
 const partial_layout = path.join(__dirname, 'views/partials')
 const public = path.join(__dirname, 'public')
-// console.log(public)
 
-app.use(express.static('public'));
+var indexRouter = require('./routes/index');
 
-const hbsE = exp_hbs.create({
+app.use(express.static(public));
+
+const handle = exp_hbs.create({
     extname: 'hbs',
     defaultLayout: 'main',
     layoutsDir: main_layout,
@@ -24,7 +25,6 @@ const hbsE = exp_hbs.create({
         list: function (value, options) {
             console.log("Passing Values", value);
             return '<h3>' + options.fn({ test: value, another: 'thing' }) + '</h3>'
-
         },
         people: function (value, options) {
             //value = people
@@ -33,91 +33,92 @@ const hbsE = exp_hbs.create({
                 out = out + '<p>' + options.fn(value[i]) + '</p>'
             }
             return out + '</ul>'
-
-
         }
     }
 })
 
-app.engine('hbs', hbsE.engine)
+app.engine('hbs', handle.engine)
 app.set('view engine', 'hbs')
 
+app.use('/', indexRouter)
 
-
-app.get('/', function (req, res) {
-    // res.status(200).json({ message: 'Welcome to Home' })
-    res.render('index', {
-        style: 'index.css',
-        title: 'HomePage',
-        isTrue: true,
-        connectedUser: false,
-
-        duoCurly: '<h3>Welcome to Duo Curly</h3>',
-        trioCurly: '<h3>Welcome to Trio Curly</h3>',
-        author: {
-            firstname: 'Malory',
-            lastname: 'Archer',
-            employees: {
-                first: 'Sterling Archer',
-                second: 'Lana Kane',
-                third: 'Cyril Figgis'
-            }
-        },
-    })
-})
-
-app.get('/about', function (req, res) {
-    // res.status(200).json({ message: 'Welcome to Home' })
-    res.render('about', {
-        style: 'about.css',
-        title: 'AboutPage',
-        name: 'BCXPR',
-        author: 'Shungoh'
-    })
-})
-
-app.get('/team', function (req, res) {
-    res.render('team', {
-        style: 'team.css',
-        programmers: [
-            'Martin',
-            'Ian',
-            'Luffy',
-            'Steve'
-        ],
-        accountant: {
-            name: 'Malory Archer',
-            username: 'ilovemoney',
-            office_number: 'b40',
-            phone: '0785746334'
-        },
-        people: [
-            { firstname: 'Janice', secondname: 'Kemunto' },
-            { firstname: 'James', secondname: 'Kaimoe' },
-            { firstname: 'Archie', secondname: 'Malowa' },
-        ]
-
-    })
-})
-
-app.get('/food', function (req, res) {
-    res.render('food', {
-        style: 'food.css',
-        food: [
-            { fruits: ['passion', 'orange', 'apple'] },
-            { vegetables: ['spinach', 'cauliflower', 'kales'] },
-            { junk: ['pizza', 'burger', 'sandwitch'] },
-            { beverages: ['coffee', 'tea', 'choco'] }
-        ]
-    })
-})
-
-app.get('/admin', function (req, res) {
-    res.render('admin', {
-        style: 'admin.css'
-    })
-})
 
 app.listen(PORT, function () {
     console.log('Server listening at port:', PORT)
 })
+
+module.exports = app
+
+
+// app.get('/', function (req, res) {
+//     res.render('index', {
+//         style: 'index.css',
+//         title: 'HomePage',
+//         isTrue: true,
+//         connectedUser: false,
+
+//         duoCurly: '<h3>Welcome to Duo Curly</h3>',
+//         trioCurly: '<h3>Welcome to Trio Curly</h3>',
+//         author: {
+//             firstname: 'Malory',
+//             lastname: 'Archer',
+//             employees: {
+//                 first: 'Sterling Archer',
+//                 second: 'Lana Kane',
+//                 third: 'Cyril Figgis'
+//             }
+//         },
+//     })
+// })
+
+// app.get('/about', function (req, res) {
+//     // res.status(200).json({ message: 'Welcome to Home' })
+//     res.render('about', {
+//         style: 'about.css',
+//         title: 'AboutPage',
+//         name: 'BCXPR',
+//         author: 'Shungoh'
+//     })
+// })
+
+// app.get('/team', function (req, res) {
+//     res.render('team', {
+//         style: 'team.css',
+//         programmers: [
+//             'Martin',
+//             'Ian',
+//             'Luffy',
+//             'Steve'
+//         ],
+//         accountant: {
+//             name: 'Malory Archer',
+//             username: 'ilovemoney',
+//             office_number: 'b40',
+//             phone: '0785746334'
+//         },
+//         people: [
+//             { firstname: 'Janice', secondname: 'Kemunto' },
+//             { firstname: 'James', secondname: 'Kaimoe' },
+//             { firstname: 'Archie', secondname: 'Malowa' },
+//         ]
+
+//     })
+// })
+
+// app.get('/food', function (req, res) {
+//     res.render('food', {
+//         style: 'food.css',
+//         food: [
+//             { fruits: ['passion', 'orange', 'apple'] },
+//             { vegetables: ['spinach', 'cauliflower', 'kales'] },
+//             { junk: ['pizza', 'burger', 'sandwitch'] },
+//             { beverages: ['coffee', 'tea', 'choco'] }
+//         ]
+//     })
+// })
+
+// app.get('/admin', function (req, res) {
+//     res.render('admin', {
+//         style: 'admin.css'
+//     })
+// })
